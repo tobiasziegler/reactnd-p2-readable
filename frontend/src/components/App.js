fb9963281b107dfde0d59e15b8fc0679513c9338
David Mallard <correspondence@davidmallard.id.au>
2017-11-21 11:17:05 +1100

    Change code format by running Prettier for the first time

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-      </div>
-    );
+    return <div className="App" />;
   }
 }
 
