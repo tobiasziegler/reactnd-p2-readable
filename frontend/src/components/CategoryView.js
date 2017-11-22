341c39d50936a98d622c7336f8c42a4054395d82
David Mallard <correspondence@davidmallard.id.au>
2017-11-23 09:52:51 +1100

    Create the view to display all posts in a category

--- frontend/src/components/CategoryView.js
+++ frontend/src/components/CategoryView.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getAllPosts, getAllCategories } from '../actions';
+import { Grid } from 'semantic-ui-react';
+import PostList from './PostList';
+import CategoryList from './CategoryList';
 
 class CategoryView extends Component {
+  componentDidMount = () => {
+    this.props.dispatch(getAllPosts());
+    this.props.dispatch(getAllCategories());
+  };
+
   render() {
-    console.log(this.props.match.params);
+    const { posts, categories } = this.props;
+
     return (
-      <p>
-        This component will display the category view for the
-        {' ' + this.props.match.params.category} category
-      </p>
+      <Grid>
+        <PostList posts={posts} />
+        <CategoryList categories={categories} />
+      </Grid>
     );
   }
 }
 
-export default CategoryView;
+function mapStateToProps({ posts, categories }, { match }) {
+  return {
+    posts: posts.filter(post => post.category === match.params.category),
+    categories
+  };
+}
+
+export default connect(mapStateToProps)(CategoryView);
