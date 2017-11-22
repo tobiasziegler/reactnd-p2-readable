2d86320a8dcf14f84518df74c812a99fc4b0cf9a
David Mallard <correspondence@davidmallard.id.au>
2017-11-23 10:27:11 +1100

    Create the view to display a single post

--- frontend/src/components/PostDetailView.js
+++ frontend/src/components/PostDetailView.js
@@ -1,14 +1,56 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getAllPosts, getAllCategories } from '../actions';
+import { Grid, Item } from 'semantic-ui-react';
+import moment from 'moment';
+import CategoryList from './CategoryList';
 
 class PostDetailView extends Component {
+  componentDidMount = () => {
+    this.props.dispatch(getAllPosts());
+    this.props.dispatch(getAllCategories());
+  };
+
   render() {
+    const { post, categories } = this.props;
+
     return (
-      <p>
-        This component will display the post detail view for post_id
-        {' ' + this.props.match.params.post_id}
-      </p>
+      <Grid>
+        <Item.Group relaxed>
+          {post && (
+            <Item key={post.id}>
+              <Item.Content>
+                <Item.Header>{post.title}</Item.Header>
+                <Item.Description>{post.body}</Item.Description>
+                <Item.Meta>
+                  {moment(post.timestamp).fromNow()} by {post.author} in{' '}
+                  {post.category}
+                </Item.Meta>
+                <Item.Extra>
+                  Votes: {post.voteScore} | Comments: {post.commentCount}
+                </Item.Extra>
+              </Item.Content>
+            </Item>
+          )}
+          {!post && (
+            <Item>
+              <Item.Content>
+                <Item.Header>Post not found.</Item.Header>
+              </Item.Content>
+            </Item>
+          )}
+        </Item.Group>
+        <CategoryList categories={categories} />
+      </Grid>
     );
   }
 }
 
-export default PostDetailView;
+function mapStateToProps({ posts, categories }, { match }) {
+  return {
+    post: posts.filter(post => post.id === match.params.post_id)[0],
+    categories
+  };
+}
+
+export default connect(mapStateToProps)(PostDetailView);
