5e3a0cbe1675f6ed01f79a2fd2b16c720d938bf0
David Mallard <correspondence@davidmallard.id.au>
2017-11-22 15:39:12 +1100

    Display all posts in the default view

--- frontend/src/components/DefaultView.js
+++ frontend/src/components/DefaultView.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getAllPosts } from '../actions';
+import { Item } from 'semantic-ui-react';
+import moment from 'moment';
 
 class DefaultView extends Component {
   componentDidMount = () => {
@@ -8,7 +10,34 @@ class DefaultView extends Component {
   };
 
   render() {
-    return <p>This component will display the default (root) view</p>;
+    const { posts } = this.props;
+    return (
+      <Item.Group relaxed>
+        {posts &&
+          posts.length > 0 &&
+          posts.map(post => (
+            <Item key={post.id}>
+              <Item.Content>
+                <Item.Header>{post.title}</Item.Header>
+                <Item.Meta>
+                  {moment(post.timestamp).fromNow()} by {post.author} in{' '}
+                  {post.category}
+                </Item.Meta>
+                <Item.Extra>
+                  Votes: {post.voteScore} | Comments: {post.commentCount}
+                </Item.Extra>
+              </Item.Content>
+            </Item>
+          ))}
+        {(!posts || posts.length === 0) && (
+          <Item>
+            <Item.Content>
+              <Item.Header>No posts found.</Item.Header>
+            </Item.Content>
+          </Item>
+        )}
+      </Item.Group>
+    );
   }
 }
 
