f04e152b837a9881724a87a7ba86ddeb33ebf343
David Mallard <correspondence@davidmallard.id.au>
2017-11-22 14:16:43 +1100

    Add a page header

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,11 +3,15 @@ import { Route } from 'react-router-dom';
 import DefaultView from './DefaultView';
 import CategoryView from './CategoryView';
 import PostDetailView from './PostDetailView';
+import { Header } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header as="h1" block>
+          Readable
+        </Header>
         <Route exact path="/" component={DefaultView} />
         <Route exact path="/:category" component={CategoryView} />
         <Route exact path="/:category/:post_id" component={PostDetailView} />
